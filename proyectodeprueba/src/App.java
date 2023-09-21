52e252d97f30aeade67184e0c98aaea0462c5fc8
DESKTOP-UFI4B32\daw1 <fmanu@outlook.es>
2023-09-21 17:09:42 +0200

    Área Triángulo Equilátero

--- proyectodeprueba/src/App.java
+++ proyectodeprueba/src/App.java
@@ -3,3 +3,15 @@ public class App {
         System.out.println("Hola, Pablo");
     }
 }
+import java.util.Scanner;
+class AreaDeTrianguloRectangulo
+{
+    public static void main(String args[])
+    {
+        Scanner s= new Scanner(System.in);
+        System.out.println("Introduce el lado del Triángulo:");
+        double a= s.nextDouble();
+        double  area=(Math.sqrt(3)/4)*(a*a);
+        System.out.println("El área del triángulo es:" + area);
+    }
+}
